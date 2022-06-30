8e812e9d1660794454f7b32632371d6a5bcca7d7
julin33 <418736745@qq.com>
2022-06-30 17:22:42 +0800

    实验室 commit

--- mybatis-01/src/main/java/com/julin/dao/UserMapper.java
+++ mybatis-01/src/main/java/com/julin/dao/UserMapper.java
@@ -3,10 +3,19 @@ package com.julin.dao;
 import com.julin.pojo.User;
 
 import java.util.List;
+import java.util.Map;
 
 public interface UserMapper {
     //查询全部用户
     List<User> getUserList();
     //根据id查询
     User getUserById(int id);
+    //insert一个用户
+    int addUser(User user);
+    User addUser2(Map<String,Object> map);
+    //修改用户
+    int updateUser(User user);
+    //删除用户
+    int deleteUser(int id);
+
 }
--- mybatis-01/src/test/java/com/julin/dao/UserMapperTest.java
+++ mybatis-01/src/test/java/com/julin/dao/UserMapperTest.java
@@ -5,7 +5,9 @@ import com.julin.utils.MybatisUtils;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class UserMapperTest {
     @Test
@@ -27,6 +29,64 @@ public class UserMapperTest {
             System.out.println(user);
         }
         //关闭SqlSession
+        sqlSession.close();
+    }
+    @Test
+    public void getUserById() {
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+
+        System.out.println(mapper.getUserById(1));
+
+        sqlSession.close();
+    }
+
+    //注意：增删改需要提交事务
+    @Test
+    public void addUser() {
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+
+        int res = mapper.addUser(new User(4, "haha", "0090993"));
+        if (res>0){
+            System.out.println("插入成功");
+        }
+        //提交事务
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+    @Test
+    public void addUser2(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        Map<String, Object> map = new HashMap<String,Object>();
+        map.put("userId","5");
+        map.put("userName","弱智");
+        map.put("password","1919810");
+
+        mapper.addUser2(map);
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+    @Test
+    public void updateUser(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        mapper.updateUser(new User(4,"傻逼","114514"));
+
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+    @Test
+    public void deleteUser(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        mapper.deleteUser(4);
+
+        sqlSession.commit();
         sqlSession.close();
     }
 }
